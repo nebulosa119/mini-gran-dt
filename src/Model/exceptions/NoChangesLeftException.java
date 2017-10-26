2b8b36dad38d665bd20e21f34f3fe3d9f2d75863
pvedoya <pvedoya@itba.edu.ar>
2017-10-26 15:04:45 -0300

    Added new exception

---
+++ src/Model/exceptions/NoChangesLeftException.java
@@ -0,0 +1,7 @@
+package Model.exceptions;
+
+///Excepcion para cuando un usuario quiere cambiar jugadores en el equipo pero se  agotaron los cambios
+
+public class NoChangesLeftException extends Exception{
+    public NoChangesLeftException(){super();}
+}
