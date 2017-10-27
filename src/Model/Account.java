02587b610528353d2fbe97918a17c61880d0a280
Emilio Basualdo <Pilobas97@gmail.com>
2017-10-27 12:08:48 -0300

    fucnioes para tom

--- src/Model/Account.java
+++ src/Model/Account.java
@@ -5,5 +5,11 @@ public abstract class Account extends Identifiable {
     public Account(String name) {
         super(name);
     }
+    public boolean isUser(){
+        return this instanceof User;
+    }
+    public boolean isAdmin(){
+        return this instanceof User;
+    }
 
 }
