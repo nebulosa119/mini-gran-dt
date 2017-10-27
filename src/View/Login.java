91fc197394192a803a23736ed79e0648ae4403a2
Tomas Dorado <tdorado@itba.edu.ar>
2017-10-27 10:47:23 -0300

    Login added and Controller cleaned

---
+++ src/View/Login.java
@@ -0,0 +1,26 @@
+package View;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Login extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
