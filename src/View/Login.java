6ff4b963cb09c05aa6d33d47f13e3bdcc1fefc59
Tomas Dorado <tdorado@itba.edu.ar>
2017-10-27 11:07:03 -0300

    Added icon

--- src/View/Login.java
+++ src/View/Login.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -11,10 +12,16 @@ public class Login extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+
         primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.getIcons().add(new Image("assets/icon.png"));
+        primaryStage.setTitle("Mini Gran DT");
+
 
         Scene scene = new Scene(root);
+
         primaryStage.setScene(scene);
         primaryStage.show();
     }
