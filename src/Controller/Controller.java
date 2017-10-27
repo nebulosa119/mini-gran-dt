a8d4830014a080061179e1912db222610660a567
Tomas Dorado <tdorado@itba.edu.ar>
2017-10-27 15:55:50 -0300

    Add LoginController

---
+++ src/Controller/Controller.java
@@ -0,0 +1,25 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private void handleClose(MouseEvent event){
+        System.exit(0);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+
+    }
+}
