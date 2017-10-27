117ad928ce8a176435a27fe2804cd586888b6a83
Emilio Basualdo <Pilobas97@gmail.com>
2017-10-27 19:19:10 -0300

    main

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,23 @@ import View.Login;
 public class Main {
 
     public static void main(String[] args) {
-
+        // cargamos usuario
         AccountsMananger<User> users = new AccountsMananger<User>();
         users.loadUsers();
-
+        // cargamos admins
         AccountsMananger<Administrator> admins = new AccountsMananger<Administrator>();
         admins.loadAdmins();
 
-        LoginWindow.main();
-        String username = Login.getUserName;
-        String type = Login.getType();// con un selecionador tipo de paises
-
-        LogInWindow.exit();
+        LogInWindow login = new LoginWindow();
+        login.main();
+        String username = login.getUserName; // login deberia chequear que haya info
+        Types type = login.getType();// con un selecionador tipo de paises
+        // recibo el username, no verificar si existe o no
+        login.exit();
 
         /////////////////////////////////////////////
         /// esto quedó horrible, hay que cambiarlo
+
         if (users.contains(username)){
             User user = (User)users.getAccount(username);
             UserWindow.main(user);
@@ -30,7 +32,7 @@ public class Main {
             Administrator admin = (Administrator) admins.getAccount(username);
             AdminWindow.main(admin);
         }else{
-            if (type.equals(Types.USER.name)){
+            if (type.equals(Types.USER)){
                 User user = new User(username);
                 users.add(user);
                 UserWindow.main(user);}
