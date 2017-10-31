9972a65bf7d658e5065ba8f60dc568d0367c4731
pmomesso <32001099+pmomesso@users.noreply.github.com>
2017-10-31 12:34:43 -0300

    Por ahora los equipos a los usuarios los mostramos en forma de tabs en un tabpane. Hay que corregir una cosa: antes de mostrarle el view para manejo de equipos a un usuario necesitamos una manera para que elija el torneo en el que va a participar.

--- src/Controller/RankingsWindow.java
+++ src/Controller/RankingsWindow.java
@@ -7,7 +7,7 @@ public class RankingsWindow extends VBox {
     private final String rules = "Da ranks";
     private Label label = new Label();
 
-    public RulesWindow() {
+    public RankingsWindow() {
         getChildren().addAll(label);
     }
 
--- src/Controller/TeamController.java
+++ src/Controller/TeamController.java
@@ -1,15 +1,18 @@
 package Controller;
 
+import Model.Player;
 import Model.Team;
+import Model.Tournament;
 import Model.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBar;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -19,10 +22,41 @@ public class TeamController {//  crea la ventana del user
 
     @FXML
     private Button exitButton, ruleButton, playerRankingButton, addPlayerButton, removePlayerButton;
-    private User u;
+    @FXML
+    private TabPane teamsTabPanes;
 
-    public void initModel(User u) {
+    private User u;
+    private Tournament t;
+
+    public void initModel(User u, Tournament t) {
+        /**Inicializo variables de instancia*/
         this.u = u;
+        this.t = t;
+
+        /**Configuro los tabs*/
+        for(Team team : t.getTeams()) {
+            Tab tab = new Tab();
+            tab.setText(team.getName());
+
+            /**Defino la tabla de jugadores que va adentro del tab particular*/
+            TableView<Player> playerTableView = new TableView<>();
+
+            /**Agrego las columnas a la tabla*/
+            TableColumn playerName = new TableColumn("Player name");
+            TableColumn playerRanking = new TableColumn("Player ranking");
+            playerTableView.getColumns().addAll(playerName, playerRanking);
+
+            /**Defino la data que va a ir adrento de la tabla*/
+            ObservableList<Player> data = FXCollections.observableArrayList();
+            for(Player p : team.getPlayers()) {
+                data.add(p);
+            }
+
+            /**Asocio los datos con las celdas de la tabla*/
+
+            teamsTabPanes.getTabs().add(tab);
+        }
+        /**Seteo los listeners de los botones*/
         exitButton.setOnAction(exitHandler);
         ruleButton.setOnAction(ruleHandler);
         playerRankingButton.setOnAction(rankingHandler);
@@ -55,7 +89,7 @@ public class TeamController {//  crea la ventana del user
         @Override
         public void handle(Event event) {
             /**Abre la ventana de ranking de los jugadores*/
-            Stage aux = getEventStage();
+            Stage aux = getEventStage(event);
             RankingsWindow rw = new RankingsWindow();
             aux.setScene(new Scene(rw));
         }
