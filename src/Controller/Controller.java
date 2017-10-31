44d107ded2b3b193f4b0a0222215340e895b0300
pmomesso <32001099+pmomesso@users.noreply.github.com>
2017-10-30 23:47:30 -0300

    Avanzo en metodo de manejo de eventos

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -24,6 +24,7 @@ import java.util.Collections;
 
 
 public class Controller extends Application {
+
     private Stage stage;
     private View view;
     private AccountsManager accounts;
@@ -94,6 +95,7 @@ public class Controller extends Application {
             System.out.println("es admin");
         }
     }
+
     @Override
     public void stop() throws Exception {
         accounts.save();
